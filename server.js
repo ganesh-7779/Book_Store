b4c44ce1d5e71b7256725d127f90d928888aab93
Ganesh Gavhad <ganeshgavhad9@gmail.com>
2021-10-25 11:39:20 +0530

    feat: Added dotenv and setup server of book store app

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT;
 
+app.use(express.urlencoded({ extended: true }));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({ Welcome: "Welcome to the Book Store App !" });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running at port no ${PORT}`);
+  });
+
+  module.exports = app;
