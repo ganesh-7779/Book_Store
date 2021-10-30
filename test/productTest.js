9cf51f26bc58323516cc91e18f08b01852ebc090
Ganesh Gavhad <ganeshgavhad9@gmail.com>
2021-10-30 23:39:56 +0530

    feat: Added test cases for deleteBook API

--- test/productTest.js
+++ test/productTest.js
@@ -64,63 +64,87 @@ chai.use(chaiHttp);
 //       });
 //    });
 // });
-describe("Update Book API", () => {
-    it("WhenGiven_InfoCorrect_BookShould_UpdatedSuccessfully", (done) => {
+// describe("Update Book API", () => {
+//     it("WhenGiven_InfoCorrect_BookShould_UpdatedSuccessfully", (done) => {
+//       const token = data.Book.validToken.token;
+//       const updateBook = {
+//         name: faker.lorem.word(),
+//         author: faker.name.findName(),
+//         category: faker.lorem.word(),
+//         description: faker.lorem.sentence(),
+//         price: 455,
+//       };
+//       chai
+//         .request(server)
+//         .put("/updateBook/617cb141e1686d2b8a417ff8")
+//         .set({ authorization: token })
+//         .send(updateBook)
+//         .end((error, res) => {
+//           res.should.have.status(201);
+//           done();
+//         });
+//     });
+//     it("WhenGiven_InfoInCorrect_shouldReturn _BookNotFound", (done) => {
+//       const token = data.Book.validToken.token;
+//       const updateBook = {
+//         name: faker.lorem.word(),
+//         author: faker.name.findName(),
+//         category: faker.lorem.word(),
+//         description: faker.lorem.sentence(),
+//         price: 455,
+//       };
+//       chai
+//         .request(server)
+//         .put("/updateBook/617cb499c8ab42b592227f52")
+//         .set({ authorization: token })
+//         .send(updateBook)
+//         .end((error, res) => {
+//           res.should.have.status(404);
+//           done();
+//         });
+//     });
+//     it("WhenGiven_TokenInvalid_ShouldReturn_InvalidToken", (done) => {
+//       const token = data.Book.inValidToken.token;
+//       const updateBook = {
+//         name: faker.lorem.word(),
+//         author: faker.name.findName(),
+//         category: faker.lorem.word(),
+//         description: faker.lorem.sentence(),
+//         price: 455,
+//       };
+//       chai
+//         .request(server)
+//         .put("/updateBook/617cb499c8ab42b592227f50")
+//         .set({ authorization: token })
+//         .send(updateBook)
+//         .end((error, res) => {
+//           res.should.have.status(400);
+//           done();
+//         });
+//      });
+//   });
+  
+describe("Delete Book API", () => {
+    it("WhenGiven_InfoCorrect_BookShould_DeleteSuccessfully", (done) => {
       const token = data.Book.validToken.token;
-      const updateBook = {
-        name: faker.lorem.word(),
-        author: faker.name.findName(),
-        category: faker.lorem.word(),
-        description: faker.lorem.sentence(),
-        price: 455,
-      };
       chai
         .request(server)
-        .put("/updateBook/617cb141e1686d2b8a417ff8")
+        .delete("/deleteBook/617bc5685612ed9ae3d457c9")
         .set({ authorization: token })
-        .send(updateBook)
         .end((error, res) => {
-          res.should.have.status(201);
+          res.should.have.status(200);
           done();
         });
     });
     it("WhenGiven_InfoInCorrect_shouldReturn _BookNotFound", (done) => {
       const token = data.Book.validToken.token;
-      const updateBook = {
-        name: faker.lorem.word(),
-        author: faker.name.findName(),
-        category: faker.lorem.word(),
-        description: faker.lorem.sentence(),
-        price: 455,
-      };
       chai
         .request(server)
-        .put("/updateBook/617cb499c8ab42b592227f52")
+        .delete("/deleteBook/617cb499c8ab42b592227f52")
         .set({ authorization: token })
-        .send(updateBook)
         .end((error, res) => {
           res.should.have.status(404);
           done();
         });
     });
-    it("WhenGiven_TokenInvalid_ShouldReturn_InvalidToken", (done) => {
-      const token = data.Book.inValidToken.token;
-      const updateBook = {
-        name: faker.lorem.word(),
-        author: faker.name.findName(),
-        category: faker.lorem.word(),
-        description: faker.lorem.sentence(),
-        price: 455,
-      };
-      chai
-        .request(server)
-        .put("/updateBook/617cb499c8ab42b592227f50")
-        .set({ authorization: token })
-        .send(updateBook)
-        .end((error, res) => {
-          res.should.have.status(400);
-          done();
-        });
-     });
   });
-  
