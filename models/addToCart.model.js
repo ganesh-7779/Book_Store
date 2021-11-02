04fea87d2ed9200a6d35c5e52d342a02b4f691f3
Ganesh Gavhad <ganeshgavhad9@gmail.com>
2021-11-02 12:50:01 +0530

    refactor: implemeted Add to card API using array of object

--- models/addToCart.model.js
+++ models/addToCart.model.js
@@ -1,46 +1,159 @@
+// const mongoose = require("mongoose");
+
+// const addtocart = mongoose.Schema({
+//   userId: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     required: true,
+//     ref: "Registration",
+//   },
+//   bookId: {
+//     type: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+//   },
+//   addedDate: {
+//     type: Date,
+//     default: Date.now(),
+//   },
+// });
+
+// const cart = mongoose.model("cart", addtocart);
+// class CartModel {
+//   addTocart = async (bookId, userId) => {
+//     const addToCartData = await cart.findOne({ userId: userId });
+//     if (addToCartData !== null) {
+//       return addToCartData;
+//     } else {
+//       const addToCartData = new cart({
+//         userId: userId,
+//         bookId: bookId,
+//       });
+//       const datauser = async () => {
+//         await addToCartData.save();
+//       };
+//       datauser();
+//       return null;
+//       // return addToCartData
+//     }
+//   };
+//   addItemTocart = async (bookId, userId) => {
+//     const data = await cart.findOneAndUpdate(
+//       { userId: userId },
+//       { $push: { bookId: bookId } },
+//       { new: true }
+//     );
+//     return data;
+//   };
+// }
+// module.exports = new CartModel();
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const addtocart = mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "Registration",
+let ItemSchema = new Schema(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      //min: [1, "Quantity can not be less then 1."],
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
   },
-  bookId: {
-    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
-  },
-  addedDate: {
-    type: Date,
-    default: Date.now(),
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+//module.exports = mongoose.model("item", ItemSchema);
+const item = mongoose.model("item", ItemSchema);
 
-const cart = mongoose.model("cart", addtocart);
-class CartModel {
-  addTocart = async (bookId, userId) => {
-    const addToCartData = await cart.findOne({ userId: userId });
-    if (addToCartData !== null) {
-      return addToCartData;
-    } else {
-      const addToCartData = new cart({
+const CartSchema = new Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Registration",
+    },
+
+    items: [ItemSchema],
+
+    subTotal: {
+      default: 0,
+      type: Number,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Cart = mongoose.model("cart", CartSchema);
+
+//module.exports = mongoose.model("cart", CartSchema);
+const product = require("../models/product.model");
+
+class cartModel {
+  addTocart = async (userId, itemId, quantity) => {
+    const cartdata = await Cart.findOne({ userId: userId });
+    const productDetails = await product.getBookById(itemId); //findById(itemId);
+
+    //return cartdata
+    if (cartdata) {
+      let indexFound = cartdata.items.findIndex((p) => p.productId == itemId);
+      console.log("Index", indexFound);
+      //check if product exist,just add the previous quantity with the new quantity and update the total price
+      if (indexFound != -1) {
+        cartdata.items[indexFound].quantity = cartdata.items[indexFound].quantity + quantity;
+        cartdata.items[indexFound].total = cartdata.items[indexFound].quantity * productDetails.price;
+        cartdata.items[indexFound].price = productDetails.price;
+        cartdata.subTotal = cartdata.items.map((item) => item.total).reduce((acc, curr) => acc + curr);
+      }
+      //Check if Quantity is Greater than 0 then add item to items Array
+      else if (quantity > 0) {
+        cartdata.items.push({
+          productId: itemId,
+          quantity: quantity,
+          price: productDetails.price,
+          total: parseInt(productDetails.price * quantity).toFixed(2),
+        });
+        cartdata.subTotal = cartdata.items
+          .map((item) => item.total)
+          .reduce((acc, curr) => acc + curr);
+      } else if(quantity < 0){
+        cartdata.pop(item)
+      }
+    
+      //if quantity of price is 0 throw the error
+      else {
+        return null;
+      }
+      const data = await cartdata.save();
+      return data;
+    }
+    //if there is no user with a cart then it creates a new cart and then adds the item to the cart that has been created
+    else {
+      const cartData = {
         userId: userId,
-        bookId: bookId,
-      });
-      const datauser = async () => {
-        await addToCartData.save();
+        items: [
+          {
+            productId: itemId,
+            quantity: quantity,
+            total: parseInt(productDetails.price * quantity),
+            price: productDetails.price,
+          },
+        ],
+        subTotal: parseInt(productDetails.price * quantity),
       };
-      datauser();
-      return null;
-      // return addToCartData
+      let cart = new Cart(cartData);
+      let data = await cart.save();
+      return data;
     }
   };
-  addItemTocart = async (bookId, userId) => {
-    const data = await cart.findOneAndUpdate(
-      { userId: userId },
-      { $push: { bookId: bookId } },
-      { new: true }
-    );
-    return data;
-  };
 }
-module.exports = new CartModel();
+
+module.exports = new cartModel();
--- service/cart.service.js
+++ service/cart.service.js
@@ -1,18 +1,23 @@
 const cartModel = require("../models/addToCart.model");
 class cartService {
-  addToCart = async (bookId, userId) => {
-    const checkToCart = await cartModel.addTocart(bookId, userId);
-    console.log(checkToCart + "cart service from modole");
-    if (checkToCart == null) {
-      return false;
-    } else {
-      for (let i = 0; i < checkToCart.bookId.length; i++) {
-        if (JSON.stringify(checkToCart.bookId[i]) === JSON.stringify(bookId)) {
-          return true;
-        }
-      }
-    }
-    return await cartModel.addItemTocart(bookId, userId);
+  // addToCart = async (bookId, userId) => {
+
+  //   const checkToCart = await cartModel.addTocart(bookId, userId);
+  //   console.log(checkToCart + "cart service from modole");
+  //   if (checkToCart == null) {
+  //     return false;
+  //   } else {
+  //     for (let i = 0; i < checkToCart.bookId.length; i++) {
+  //       if (JSON.stringify(checkToCart.bookId[i]) === JSON.stringify(bookId)) {
+  //         return true;
+  //       }
+  //     }
+  //   }
+  //   return await cartModel.addItemTocart(bookId, userId);
+  // };
+  addToCartSer = async (userId,itemId,quantity) => {
+    const checkToCart = await cartModel.addTocart(userId,itemId,quantity);
+    return checkToCart
   };
 }
 module.exports = new cartService();
