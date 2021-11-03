0e72d0e4e7854fd544da51ee743161d904e8bcea
Ganesh Gavhad <ganeshgavhad9@gmail.com>
2021-11-03 21:52:34 +0530

    feat: Added RemoveFromCart API and Some code changes

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,59 +1,51 @@
 const cartService = require("../service/cart.service");
 const bookModel = require("../models/product.model");
 const product = require("../models/product.model");
-const cartModel = require("../models/addToCart.model")
-const logger = require("../logger/logger")
+const cartModel = require("../models/addToCart.model");
+const logger = require("../logger/logger");
 class CartController {
-  // addToCart = async (req, res) => {
-  //   const userId = req.user.dataForToken.id,
-  //     bookId = req.params.Id;
-  //   console.log(userId, bookId);
 
-  //   const checkBook = await bookModel.getBookById(bookId);
-  //   if (!checkBook) {
-  //     return res.status(404).json({
-  //       message: "Book Already Sold Out",
-  //       success: false,
-  //     });
-  //   } else {
-  //     const isExist = await cartService.addToCart(bookId, userId);
-  //     console.log(isExist);
-  //     if (isExist === true) {
-  //       return res.status(201).json({
-  //         message: "Book Already Added To Cart",
-  //         success: false,
-  //       });
-  //     } else if (isExist === false) {
-  //       return res.status(201).json({
-  //         message: "Book SuccessFully Added To Cart",
-  //         success: true,
-  //         data: isExist,
-  //       });
-  //     } else {
-  //       return res.status(201).json({
-  //         message: "Book SuccessFully Added To Cart",
-  //         success: true,
-  //         data: isExist,
-  //       });
-  //     }
-  //   }
-  // };
   addToCart = async (req, res) => {
     const { userId, itemId } = req.body;
     const quantity = Number.parseInt(req.body.quantity);
-    let cart = await cartService.addToCartSer(userId,itemId,quantity);
+    let cart = await cartService.addToCartSer(userId, itemId, quantity);
     if (cart) {
-      logger.info("Add to Cart successfully")
+      logger.info("Add to Cart successfully");
       res.status(200).send({
         message: "Add to Cart successfully!",
         data: cart,
       });
-    }else{
-    logger.info("Invalid request,Please enter valid quantity")
-    return res.status(400).json({
-      message: "Invalid request,Please enter valid quantity",
-    });
+    } else {
+      logger.info("Invalid request,Please enter valid quantity");
+      return res.status(400).json({
+        message: "Invalid request,Please enter valid quantity",
+      });
+    }
   };
-}
+  removeFromCart = async (req, res) => {
+    const { userId, bookId } = req.body;
+    let cart = await cartService.removeToCart(userId, bookId);
+    if (cart) {
+      logger.info("Remove From Cart successfully");
+      res.status(200).send({
+        message: "Remove From  Cart successfully!",
+        data: cart,
+        success: true,
+
+      });
+    } else if(cart == false){
+      res.status(404).send({
+        message: "Cart Not Found",
+        success: false
+      });
+    }
+    else{
+      res.status(404).send({
+        message: "Book Not Found",
+        data: cart,
+        success: false
+      });
+    }
+  };
 }
 module.exports = new CartController();
