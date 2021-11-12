1426d63615e3dede70982cdb7c364f8b32835b21
Ganesh Gavhad <ganeshgavhad9@gmail.com>
2021-11-13 00:24:10 +0530

    feat: Added test cases get book by highTolowPrice API

--- test/getAllBookTest.js
+++ test/getAllBookTest.js
@@ -75,3 +75,38 @@ describe("Get All Book With Low To High Price API", () => {
        });
     });
 });
+describe("Get All Book With High To Low Price API", () => {
+    it("WhenGiven_InfoCorrect_AllBookShould_ReturnSuccessfully", (done) => {
+      const token = data.Book.validToken.token;
+      chai
+        .request(server)
+        .get("/highToLowPrice")
+        .set({ authorization: token })
+        .end((error, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+    it("WhenGiven_InfoInCorrect_shouldReturn_BookNotFound", (done) => {
+      const token = data.Book.validWithoutCart.token;
+      chai
+        .request(server)
+        .get("/highToLowPrice")
+        .set({ authorization: token })
+        .end((error, res) => {
+          res.should.have.status(404);
+          done();
+        });
+    });
+    it("WhenGiven_InfoInCorrect_shouldReturn_invalidToken", (done) => {
+      const token = data.Book.inValidToken.token;
+      chai
+        .request(server)
+        .get("/highToLowPrice")
+        .set({ authorization: token })
+        .end((error, res) => {
+          res.should.have.status(400);
+          done();
+       });
+    });
+});
